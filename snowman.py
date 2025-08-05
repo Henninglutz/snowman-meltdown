d809ed63eb819a3b333fce0a8a12f2ddc3355953
Henninglutz <henninglutztepe@gmail.com>
2025-08-05 12:46:36 +0200

    Create .gitignore

--- snowman.py
+++ snowman.py
@@ -14,7 +14,20 @@ def play_game():
     print("Welcome to Snowman Meltdown!")
     print("Secret word selected: " + secret_word)  # for testing, later remove this line
 
-    # TODO: Build your game loop here.
+    for guess in secret_word:
+        if guess in secret_word:
+            #for char in secret_word:
+            print(f"_"* len(secret_word))
+
+    """
+    Loop Print  "_" for every letter 
+    (neue Variable mit den bereits geratenen Buchstaben = neuer String) 
+    guess print für neuen String in der richtigen Stelle 
+    (if guess in secret word guess ???!index im string? replace index mit guess:
+    else: print "_")
+    Erst Index() = secret_word.index(guess)
+    Variabele updaten = dann replace()
+
     # For now, simply prompt the user once:
     guess = input("Guess a letter: ").lower()
     print("You guessed:", guess)
