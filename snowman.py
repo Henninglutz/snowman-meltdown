d002d70c17199af883e877b59dfd6fed82c951a8
Henninglutz <henninglutztepe@gmail.com>
2025-08-11 14:50:46 +0200

    Update snowman.py

--- snowman.py
+++ snowman.py
@@ -2,7 +2,8 @@ import random
 
 # List of secret words
 WORDS = ["python", "git", "github", "snowman", "meltdown"]
-
+mistake =  0
+word_progress = ""
 
 def get_random_word():
     """Selects a random word from the list."""
@@ -13,11 +14,14 @@ def play_game():
     secret_word = get_random_word()
     print("Welcome to Snowman Meltdown!")
     print("Secret word selected: " + secret_word)  # for testing, later remove this line
+    display_game_state()
 
     for guess in secret_word:
         if guess in secret_word:
             #for char in secret_word:
             print(f"_"* len(secret_word))
+        else:
+            mistake += 1
 
     """
     Loop Print  "_" for every letter 
@@ -31,7 +35,8 @@ def play_game():
     # For now, simply prompt the user once:
     guess = input("Guess a letter: ").lower()
     print("You guessed:", guess)
-
+    
+"""
 
 if __name__ == "__main__":
     play_game()
